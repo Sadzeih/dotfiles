88ec0eff404324202e3dfb0602935b3f1c457f37
Alexis Guerville <sadzeih@gmail.com>
2024-03-15 12:37:38 +0100

    add more fish stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,20 @@ set -x GOPATH $HOME/go
 
 set -U fish_user_paths $HOME/go/bin $HOME/.pulumi/bin $fish_user_paths
 
+set -U EDITOR "nvim"
+set -U GIT_EDITOR "nvim"
+set -x GOPRIVATE "gitlab.com/sibros/*"
+
 alias vim="nvim"
+
+function tmux
+  set -lx TERM xterm-256color 
+  command tmux $argv
+end
+
+alias kns="kubens"
+alias kctx="kubectx"
+alias k="kubectl"
 
 alias gs="git status"
 alias gd="git diff"
