a1d2a14253213777d15e1608a40087062dbcfa8e
Alexis Guerville <alexis.guerville@mistral.ai>
2026-03-16 12:42:05 +0100

    fish

---
+++ .config/fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,43 @@
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+
+set --global fish_color_autosuggestion 908caa
+set --global fish_color_cancel e0def4
+set --global fish_color_command c4a7e7
+set --global fish_color_comment 908caa
+set --global fish_color_cwd ea9a97
+set --global fish_color_cwd_root 
+set --global fish_color_end 908caa
+set --global fish_color_error eb6f92
+set --global fish_color_escape 3e8fb0
+set --global fish_color_history_current 
+set --global fish_color_host 9ccfd8
+set --global fish_color_host_remote c4a7e7
+set --global fish_color_keyword 9ccfd8
+set --global fish_color_normal e0def4
+set --global fish_color_operator e0def4
+set --global fish_color_param ea9a97
+set --global fish_color_quote f6c177
+set --global fish_color_redirection 3e8fb0
+set --global fish_color_search_match --background=232136
+set --global fish_color_selection --reverse
+set --global fish_color_status 
+set --global fish_color_user f6c177
+set --global fish_color_valid_path 
+set --global fish_pager_color_background --background=2a273f
+set --global fish_pager_color_completion 908caa
+set --global fish_pager_color_description 908caa
+set --global fish_pager_color_prefix 9ccfd8
+set --global fish_pager_color_progress ea9a97
+set --global fish_pager_color_secondary_background 
+set --global fish_pager_color_secondary_completion 
+set --global fish_pager_color_secondary_description 
+set --global fish_pager_color_secondary_prefix 
+set --global fish_pager_color_selected_background --background=393552
+set --global fish_pager_color_selected_completion e0def4
+set --global fish_pager_color_selected_description e0def4
+set --global fish_pager_color_selected_prefix 9ccfd8
